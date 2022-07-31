ec37bd22e93472bda1364999ab2ee9547459d483
tsots <mosalatsotetsi@gmail.com>
2022-07-31 22:34:30 +0200

    add image prefix

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
+import Meta from "../components/Meta";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
+      <Meta />
       <h1>We are here</h1>
-    </div>
+    </>
   );
 }
